cb877a9ff009a74b3f21f27d024ded13e82678e3
maksimbelaev <maxim.beliaev18@gmail.com>
2026-04-16 23:14:34 +0300

    Complete Lab2

--- Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b46139b0febe1ca946018b049ddeceb497e9ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+844cc4c51fd8d341e5f57a105487f5cc6fd22d90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
