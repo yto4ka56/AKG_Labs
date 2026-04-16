74d43449b86eee411c3bc644337696653ba54344
maksimbelaev <maxim.beliaev18@gmail.com>
2026-04-17 02:14:46 +0300

    git fix finally

--- Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+844cc4c51fd8d341e5f57a105487f5cc6fd22d90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee37e4246696e8fb54dd21b52e82d5881498e58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
